dc91a1aa36fbf562757342f69f056783aea4a1c5
Andrey <systerr@gmail.com>
2023-08-28 00:14:39 +0400

    deps + tests

--- src/controllers/Person.test.js
+++ src/controllers/Person.test.js
@@ -1,11 +1,7 @@
-const request = require('supertest');
-
 describe('create', () => {
   it('creat person', async () => {
     expect.assertions(1);
-    const { status } = await request(global.server.app.httpServer.express).get(
-      '/person',
-    );
+    const { status } = await fetch(global.server.testingGetUrl('/person'));
     expect(status).toBe(200);
   });
 });
