58a32f0ad45500b801cd5a14292d702e793aca00
Andrey <systerr@gmail.com>
2025-04-23 16:57:14 +0100

    feat: migrate Person and User models to TypeScript with enhanced type safety

--- src/models/Person.ts
+++ src/models/Person.ts
@@ -1,7 +1,33 @@
 import AbstractModel from '@adaptivestone/framework/modules/AbstractModel.js';
 import Mailer from '@adaptivestone/framework-module-email';
 
-class Person extends AbstractModel {
+import type {
+  IAbstractModel,
+  IAbstractModelMethods,
+} from '@adaptivestone/framework/modules/AbstractModel.d.ts';
+
+import type { TFunction } from 'i18next';
+
+interface IPerson {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
+interface IStatic
+  extends IAbstractModel<IPerson, IAbstractModelMethods<IPerson>> {
+  getFullName(): string;
+}
+
+class Person
+  extends AbstractModel<IPerson, IAbstractModelMethods<IPerson>, IStatic>
+  implements IPerson
+{
+  // Declare properties from IPerson to satisfy TypeScript
+  firstName!: string;
+  lastName!: string;
+  email!: string;
+
   // https://mongoosejs.com/docs/advanced_schemas.html
 
   // eslint-disable-next-line class-methods-use-this
@@ -9,6 +35,7 @@ class Person extends AbstractModel {
     return {
       firstName: String,
       lastName: String,
+      email: String,
     };
   }
 
@@ -29,16 +56,19 @@ class Person extends AbstractModel {
   }
 
   // `findByFullName()` becomes a static
-  static findByFullName(name) {
+  static findByFullName(this: Person['mongooseModel'], name: string) {
     const firstSpace = name.indexOf(' ');
     const firstName = name.split(' ')[0];
     const lastName = firstSpace === -1 ? '' : name.substr(firstSpace + 1);
     return this.findOne({ firstName, lastName });
   }
 
-  async sendCreatEmail(i18n) {
+  async sendCreatEmail(
+    this: InstanceType<Person['mongooseModel']>,
+    i18n: { t: TFunction; language: string },
+  ) {
     const mail = new Mailer(
-      this.constructor.getSuper().app,
+      this.getSuper().app,
       'verification',
       {
         fullName: `${this.firstName} ${this.lastName}`,
