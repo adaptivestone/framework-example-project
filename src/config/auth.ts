71aceb27bfd9efdb14416cb906723f26f700fabd
Andrey <systerr@gmail.com>
2026-06-13 22:27:02 +0100

    update config name

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -2,5 +2,5 @@ import originalAuth from '@adaptivestone/framework/config/auth.js';
 
 export default {
   ...originalAuth,
-  isAuthWithVefificationFlow: false,
+  isAuthWithVerificationFlow: false,
 };
