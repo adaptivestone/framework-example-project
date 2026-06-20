3d68fdc869db8cb2d2841fbc55a9fb4d1cd3ff80
Andrey <systerr@gmail.com>
2026-06-20 01:37:30 +0100

    update framework

--- src/controllers/Auth.ts
+++
@@ -1,5 +0,0 @@
-import AbstractController from '@adaptivestone/framework/modules/AbstractController.js';
-
-class Auth extends AbstractController {}
-
-export default Auth;
--- src/folderConfig.ts
+++ src/folderConfig.ts
@@ -8,7 +8,6 @@ export default {
     config: path.resolve(__dirname, './config'),
     models: path.resolve(__dirname, './models'),
     controllers: path.resolve(__dirname, './controllers'),
-    views: path.resolve(__dirname, './views'),
     locales: path.resolve(__dirname, './locales'),
     emails: path.resolve(__dirname, './services/messaging/email/templates'),
     commands: path.resolve(__dirname, './commands'),
